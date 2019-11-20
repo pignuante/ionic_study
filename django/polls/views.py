c68d0bb22da0375fd6e8b259c73c773be3a21b25
pignuante <syaoran215@naver.com>
2019-11-20 10:12:42 +0900

    namespace

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -12,20 +12,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#     template = loader.get_template("polls/index.html")
-#     context = {
-#         "latest_question_list": latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
-
-
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
